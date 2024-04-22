dcb452f17adc973120eb54b41593125fded33305
Lucia <luciferma14@gmail.com>
2024-04-22 20:33:32 +0200

    Update

--- Calculator/src/test/java/org/antonio/CalculatorIntegrationTest.java
+++ Calculator/src/test/java/org/antonio/CalculatorIntegrationTest.java
@@ -1,4 +1,17 @@
 package org.antonio;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class CalculatorIntegrationTest {
+
+    @Test
+    public void testAddAndSubstract(){
+        Calculator calculator = new Calculator();
+        int result = calculator.add(1, 2);
+        assertEquals(3, result);
+
+        result = calculator.subtract(1, 2);
+
+        assertEquals(-1, result);
+    }
 }
--- Calculator/src/test/java/org/antonio/CalculatorTest.java
+++ Calculator/src/test/java/org/antonio/CalculatorTest.java
@@ -10,5 +10,39 @@ public class CalculatorTest {
         int result = calculator.add(1, 2);
 
         assertEquals(3, result);
+        assertNotEquals(5, result);
+    }
+
+    @Test
+    public void testSubstract(){
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(1, 2);
+
+        assertEquals(-1, result);
+        assertNotEquals(3, result);
+    }
+
+    @Test
+    public void testMultily(){
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(2, 3);
+
+        assertEquals(6, result);
+        assertNotEquals(3, result);
+    }
+
+    @Test
+    public void testDivide(){
+        Calculator calculator = new Calculator();
+        float result = calculator.divide(4,2);
+
+        assertEquals(2, (float)result, 0.01);
+        assertNotEquals(5, (float)result, 0.01);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero(){
+        Calculator calculator = new Calculator();
+        calculator.divide(4, 0);
     }
 }
