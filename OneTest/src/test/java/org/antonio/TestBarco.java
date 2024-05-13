d8b1d5c7eb81d69443e47bbef5a60f8136c9267f
Lucia <luciferma14@gmail.com>
2024-05-13 19:53:07 +0200

    Update

--- OneTest/src/test/java/org/antonio/TestBarco.java
+++ OneTest/src/test/java/org/antonio/TestBarco.java
@@ -1,4 +1,33 @@
 package org.antonio;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestBarco {
+
+
+    Barco barco1 = new Barco(null, 0);
+    Barco barco2 = new Barco(null, 0);
+
+    @Before
+    public void setUp(){
+        barco1 = new Barco("Thousand Sunny", 5);
+        barco2 = new Barco("Red Force", 10);
+    }
+
+    @Test
+    public void testAgregarTripulante(){
+
+        assertTrue(barco1.agregarTripulante());
+        assertEquals(6, barco1.getCapacidad());
+    }
+
+    @Test
+    public void testAgregarTripulanteMaximo(){
+
+        assertFalse(barco2.agregarTripulante());
+        assertEquals(10, barco2.getCapacidad());
+    }
 }
--- OneTest/src/test/java/org/antonio/TestFruta.java
+++ OneTest/src/test/java/org/antonio/TestFruta.java
@@ -3,5 +3,31 @@ package org.antonio;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class TestFruta {
+
+    Fruta fruta1 = new Fruta (null, false);
+    Fruta fruta2 = new Fruta (null, false);
+
+    @Before
+    public void setUp(){
+        fruta1 = new Fruta("Gomu Gomu", true);
+        fruta2 = new Fruta("Fruta del Diablo", false);
+    }
+
+    @Test
+    public void testPuedoComerFrutaDeliciosa(){
+        assertTrue(fruta1.puedoComer());
+    }
+
+    @Test
+    public void testPuedoComerFrutaDelDiablo(){
+        assertFalse(fruta2.puedoComer());
+    }
+
+    @Test
+    public void testPuedoComerFrutaNoDeliciosa(){
+        assertFalse(fruta2.puedoComer());
+    }
 }
